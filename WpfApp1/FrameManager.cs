6ffa68b658753c696d96a64513f1aa50d43ca601
slivki05 <slivki0611@gmail.com>
2023-11-21 17:29:25 +0800

    Каркас

---
+++ WpfApp1/FrameManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    static class FrameManager
+    {
+        public static System.Windows.Controls.Frame Frame;
+    }
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,17 +12,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1.Pages;
 
 namespace WpfApp1
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            FrameManager.Frame = MainFrame;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FrameManager.Frame.Navigate(new MainPage());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            FrameManager.Frame.Navigate(new Page1());
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            FrameManager.Frame.Navigate(new Page2());
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            FrameManager.Frame.Navigate(new Page3());
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            FrameManager.Frame.Navigate(new Page4());
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            FrameManager.Frame.Navigate(new Page5());
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            FrameManager.Frame.Navigate(new Page6());
         }
     }
 }
